c6298ac6e414d47e9dabb8e53cb90ae08ad21f39
DESKTOP-L6LU0BL\ajiha <aji.hanggoro@outlook.com>
2023-08-04 10:17:49 +0700

    feat: implement engagementMessageOverTimeChartOptions

---
+++ src/components/pages/EngagementMessage/EngagementHelper.ts
@@ -0,0 +1,57 @@
+import moment from "moment";
+import { Channel, MessageCount } from "./type"
+
+export const engagementMessageOverTimeChartOptions = (messageCountList: MessageCount[], channels: Channel[]) => {
+    const series: {
+        data: number[];
+        name: string;
+    }[] = []
+    const categories: string[] = []
+    for (const messageCount of messageCountList) {
+        const name = getChannelsName(channels, messageCount.channelId)
+        const date = moment(messageCount.timeBucket).format("MMM Do")
+        if (!name) continue
+        if (!series?.length) {
+            const data = [Number(messageCount.count)]
+            series.push({
+                data,
+                name
+            })
+            categories.push(date)
+            continue
+        }
+        const targetSeriesIndex = series.findIndex(serie => serie?.name === name)
+        if (targetSeriesIndex === -1) {
+            const data = [Number(messageCount.count)]
+            series.push({
+                data,
+                name
+            })
+        } else {
+            series[targetSeriesIndex].data.push(Number(messageCount.count))
+        }
+        categories.push(date)
+    }
+
+    return {
+        title: {
+            text: 'Chart Test'
+        }, 
+        series,
+        xAxis: {
+            categories
+          },
+    }
+}
+
+const getChannelsName = (channels: Channel[], channelId: string) => {
+    let channelName = ""
+
+    for (const channel of channels) {
+        if (channel.id === channelId) {
+            channelName = channel?.name
+        }
+    }
+    return channelName
+}
+
